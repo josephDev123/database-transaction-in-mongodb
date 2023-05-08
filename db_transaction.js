4e7fc96417604e9e0601ad67828d28e7e158c0c1
unknown <josephuzuegbu55@gmail.com>
2023-05-08 16:55:31 +0100

    added the final simple demostration of transaction code

--- db_transaction.js
+++ db_transaction.js
@@ -10,20 +10,37 @@ export async function signUp_profileTransaction(model1, model, user_credentials)
     //start session
     const session = await mongoose.startSession();
     try{
-    const signUp_profileTransaction = await session.withTransaction(async ()=>{
+    const transactions = await session.withTransaction( async ()=>{
         const {name, email, lastname, telephone, age, gender} = user_credentials
       
+        //all the code runs sucessful or finally. nothing like partial success
             const user = new model1({'name': name,'email': email});
-            console.log(user)
+            const profile = new model({
+                name: name,
+                lastname: lastname,
+                email: email,
+                description: 'Im Joseph  in nigeria',
+                telephone: telephone,
+                age: age,
+                gender: gender
+            })
+            
+            await user.save({session});
+            await profile.save({session});
         
     }, option)
-    
 
+    // const result = await transactions
+    console.log(transactions)
+    if(transactions){
+        console.log("The transaction was successful");
+    }
+   
     }catch(e){
-        console.log("The transaction was aborted due to an unexpected error: " + e);
+        console.log("The transaction will be aborted due to an unexpected error: " + e);
+        // session.abortTransaction();
     }finally{
-        
-        session.endSession();
+        await session.endSession();
     }
 
 }
--- routers/user_profile_router.js
+++ routers/user_profile_router.js
@@ -5,20 +5,16 @@ import {signUp_profileTransaction} from '../db_transaction.js'
 
 export const Router = express.Router()
 
-Router.post('/sign-up', (req, res)=>{
+Router.post('/sign-up', async (req, res)=>{
 const user_credential = req.body; 
 // 1.  check whether the user has already registered
 
 // 2. start transaction here
 try{
-    signUp_profileTransaction(User_model, Profile_model, user_credential);
+    await signUp_profileTransaction(User_model, Profile_model, user_credential);
     res.end()
 }catch(error){
 console.log(error)
 }
    
 });
-
-Router.get('profile', (req, res)=>{
-    
-})
